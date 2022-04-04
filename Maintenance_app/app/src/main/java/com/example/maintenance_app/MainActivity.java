b275d55fb83c4507218ac5de4a2d349b2c71a238
Erynix <nyuszoo007@gmail.com>
2022-04-04 23:48:59 +0200

    finished login and sign in

--- Maintenance_app/app/src/main/java/com/example/maintenance_app/MainActivity.java
+++ Maintenance_app/app/src/main/java/com/example/maintenance_app/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.maintenance_app;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -10,7 +11,14 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 
 public class MainActivity extends AppCompatActivity{
@@ -18,6 +26,9 @@ public class MainActivity extends AppCompatActivity{
     private Button login;
     private EditText username, password;
     private DatabaseReference dbRef;
+    private String _username, _password;
+    private String dbUsername;
+    private boolean admin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,20 +38,20 @@ public class MainActivity extends AppCompatActivity{
         username = (EditText) findViewById(R.id.username);
         password = (EditText) findViewById(R.id.password);
 
+        dbRef = FirebaseDatabase.getInstance().getReference().child("employees");
 
         login = (Button) findViewById(R.id.login);
         login.setOnClickListener(new View.OnClickListener() {
+
             @Override
             public void onClick(View view) {
-                //workerLogin(); //TODO: bejelentkezes
-                Intent intent = new Intent(MainActivity.this, AdminHome.class);
-                startActivity(intent);
-//
+                workerLogin();
             }
 
             private void workerLogin() {
-                String _username = username.getText().toString();
-                String _password = password.getText().toString();
+                
+                _username = username.getText().toString();
+                _password = password.getText().toString();
 
                 if (_username.isEmpty()){
                     username.setError("A név megadása kötelező!");
@@ -54,21 +65,48 @@ public class MainActivity extends AppCompatActivity{
                     return;
                 }
 
-                try{
+                SearchData(dbRef, _username, _password);
+            }
 
-                    //TODO: bejelentkezes... aztan abba bele:
+            private void SearchData (DatabaseReference ref , String username_, String password_){
+                ref.orderByChild("username").equalTo(username_).addValueEventListener(new ValueEventListener(){
+                    @Override
+                    public void onDataChange(DataSnapshot snapshot) {
+                        if(snapshot.exists()){
+                            dbUsername = snapshot.toString();
+                            ref.orderByChild("password").equalTo(password_).addValueEventListener(new ValueEventListener(){
+                                @Override
+                                public void onDataChange(DataSnapshot snapshot) {
+                                    if(snapshot.exists()){
+                                        Intent intent;
+                                        if (username_.equals("admin")){
+                                            intent = new Intent(MainActivity.this, AdminHome.class);
+                                            startActivity(intent);
+                                        } else {
+                                            intent = new Intent(MainActivity.this, WorkerHome.class);
+                                            startActivity(intent);
+                                        }
 
-                    if (_username.equalsIgnoreCase("admin")){
-                        Intent intent = new Intent(MainActivity.this, AdminHome.class);
-                        startActivity(intent);
-                    } else {
-                        Intent intent = new Intent(MainActivity.this, WorkerHome.class);
-                        startActivity(intent);
+                                    } else {
+                                        password.setError("Hibás jelszó!");
+                                        password.requestFocus();
+                                    }
+                                }
+
+                                @Override
+                                public void onCancelled(@NonNull DatabaseError error) {
+                                }
+                            });
+                        } else {
+                            username.setError("Hibás felhasználónév!");
+                            username.requestFocus();
+                        }
                     }
 
-                } catch (Exception e){
-                    Toast.makeText(MainActivity.this, e.getMessage(), Toast.LENGTH_LONG).show();
-                }
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+                    }
+                });
             }
         });
     }
--- Maintenance_app/app/src/main/java/com/example/maintenance_app/WorkerHome.java
+++ Maintenance_app/app/src/main/java/com/example/maintenance_app/WorkerHome.java
@@ -2,13 +2,28 @@ package com.example.maintenance_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class WorkerHome extends AppCompatActivity {
+
+    private Button logout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_worker_home);
+
+        logout = (Button) findViewById(R.id.logout2);
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(WorkerHome.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
+
 }
