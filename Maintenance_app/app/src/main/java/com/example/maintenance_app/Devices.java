8b015a3a4e4b313f0a7fe449e2e897e86d6e6a4e
Erynix <nyuszoo007@gmail.com>
2022-04-05 09:57:41 +0200

    lastlast

--- Maintenance_app/app/src/main/java/com/example/maintenance_app/Devices.java
+++ Maintenance_app/app/src/main/java/com/example/maintenance_app/Devices.java
@@ -52,6 +52,7 @@ public class Devices extends AppCompatActivity {
                 devMap.put("Category", Category);
                 devMap.put("Location", Location);
                 devMap.put("Description", Description);
+                devMap.put("status", "karbantartas szukseges");
 
                 root.push().setValue(devMap);
 
